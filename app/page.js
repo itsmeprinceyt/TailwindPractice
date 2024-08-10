9c2fb3adaa1e616095a357c051f70a881aa7f205
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-10 16:08:08 +0530

    added links

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-gradient-to-r from-[#383838] to-black min-h-screen flex flex-col justify-center items-center text-white">
-      <div className="bg-white p-5 text-black rounded-2xl">
+      <div className="bg-white p-5 text-black rounded-2xl shadow-2xl">
         <div className="mb-8 text-center">
           <p className="text-xs">Mohd Uvaish | ItsMe Prince</p>
           <p className="text-3xl font-bold">I&apos;m Learning TailwindCSS</p>
@@ -14,9 +14,15 @@ export default function Home() {
             <Link href="/ContainerColumn"
             className=" hover:text-[18px] hover:opacity-75 transition-all"
             target="_blank">Container Columns</Link>
-            <Link href="https://github.com/yourusername" className="hover:text-[18px] hover:opacity-75 transition-all" target="_blank">[Github]</Link>
+            <Link href="https://github.com/itsmeprinceyt/TailwindPractice/tree/main/app/ContainerColumn" className="hover:text-[18px] hover:opacity-75 transition-all" target="_blank">[Github]</Link>
           </div>
         </div>
+      </div>
+      <div className="mt-5 bg-white p-5 text-black rounded-full shadow-2xl hover:bg-black hover:text-white hover:p-6 transition-all">
+      <Link href="https://github.com/itsmeprinceyt/TailwindPractice"
+            
+            target="_blank">Github Repository</Link>
+        
       </div>
     </div>
   );
